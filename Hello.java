b296a3248eddb0bac41b12a956ffd8098758c884
ajaybangaru <ajaybangaru06@gmail.com>
2026-07-03 10:27:07 +0530

    Add Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins!");
+        System.out.println("Learning CI/CD with GitHub and Jenkins.");
+    }
+}
